093856eadc963a7691f6a2edbc70a3d13b1f86fb
CVBGOD <mercwar01@gmail.com>
2026-02-13 18:03:14 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,4 +1,4 @@
-; fire-gem/fire-end.asm
+; fire-end.asm
 section .data
     log_path db "fire-gem/fire-gem.log", 0
     end_msg  db "[AVIS-END] Chain Sync Complete. HAHA!", 0xa
@@ -6,23 +6,27 @@ section .data
 
 section .text
     global _start
-
 _start:
-    ; Open log in Append mode (1089)
-    mov rax, 2          ; sys_open
+    ; 1. Open log to append final signal
+    mov rax, 2
     mov rdi, log_path
-    mov rsi, 1089       
+    mov rsi, 1089
     mov rdx, 0644o
     syscall
-    
-    ; Write the terminator signal
-    mov rdi, rax
-    mov rax, 1          ; sys_write
+    mov r8, rax
+
+    mov rax, 1
+    mov rdi, r8
     mov rsi, end_msg
     mov rdx, end_len
     syscall
 
-    ; Final exit
+    ; 2. Force Disk Sync
+    mov rax, 74         ; sys_fsync
+    mov rdi, r8
+    syscall
+
+    ; 3. Kernel Shutdown
     mov rax, 60
     xor rdi, rdi
     syscall
