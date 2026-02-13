28e948d2422aa4edf25f917649e990f21affe65a
CVBGOD <mercwar01@gmail.com>
2026-02-13 18:45:21 -0500

    Update fire-macro.asm

--- fire-gem/fire-macro.asm
+++ fire-gem/fire-macro.asm
@@ -3,31 +3,28 @@
 ;  FILE: fire-macro.asm
 ; =============================================================================
 section .data
-    log_path db "fire-gem/fire-gem.log", 0
-    ; Matches the message defined in [AVIS_MACRO_CONFIG]
-    macro_msg db "[AVIS-MACRO] hello from the macro.ini", 0xa
-    macro_len equ $ - macro_msg
+    site_script db "fire-gem/fire-site.sh", 0
+    msg_bot     db "[AVIS-MACRO] Little Bot calling fire-site.sh...", 0xa
+    len_bot     equ $ - msg_bot
 
 section .text
     global _start
-
 _start:
-    ; 1. Open Log in Append Mode
-    mov rax, 2          ; sys_open
-    mov rdi, log_path
-    mov rsi, 1089       ; O_CREAT | O_WRONLY | O_APPEND
-    mov rdx, 0644o
-    syscall
-    mov r8, rax         ; Store FD
-
-    ; 2. Inject Macro Handshake
-    mov rax, 1          ; sys_write
-    mov rdi, r8
-    mov rsi, macro_msg
-    mov rdx, macro_len
+    ; 1. Log intent to stdout (GitHub Console)
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg_bot
+    mov rdx, len_bot
     syscall
 
-    ; 3. Exit back to Sequential Loader
+    ; 2. Execute the sitemap logic
+    mov rax, 59             ; sys_execve
+    mov rdi, site_script
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+
+    ; 3. Exit
     mov rax, 60
     xor rdi, rdi
     syscall
