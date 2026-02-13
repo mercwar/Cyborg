0351bf81cd22d21b8ca595bbcb3e114ff3b97686
CVBGOD <mercwar01@gmail.com>
2026-02-13 17:23:31 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# fire-gem/fire-gem.sh
-
 LOG="fire-gem/fire-gem.log"
 
-# AVIS SH-Level Logging
 echo "[AVIS-SH] Identity: fire-gem-sh" >> "$LOG"
-echo "[AVIS-SH] Status: Compiling ASM Chain..." >> "$LOG"
+echo "[AVIS-SH] Status: Building Hardware Chain..." >> "$LOG"
 
-# 1. Assemble the core
+# Build/Run Core
 nasm -f elf64 fire-gem/fire-gem.asm -o fire-gem/fire-gem.o
-
-# 2. Link to create the executable
 ld fire-gem/fire-gem.o -o fire-gem/fire-gem.exe
-
-# 3. Execute the ASM (Hardware Level)
 ./fire-gem/fire-gem.exe
+
+# Build/Run Terminator
+nasm -f elf64 fire-gem/fire-end.asm -o fire-gem/fire-end.o
+ld fire-gem/fire-end.o -o fire-gem/fire-end.exe
+./fire-gem/fire-end.exe
