8f71e73b2cec74ab733642feb32670ea22a14e34
CVBGOD <mercwar01@gmail.com>
2026-02-13 18:15:16 -0500

    Update fire-begin.sh

--- fire-gem/fire-begin.sh
+++ fire-gem/fire-begin.sh
@@ -2,12 +2,12 @@
 # fire-gem/fire-begin.sh
 set -e
 
-# Compile the new FVS Buddy and the Macro Stub
+# Compile the new FVS and Macro stubs
 nasm -f elf64 fire-gem/avis-fvs.asm -o fire-gem/avis-fvs.o
 ld fire-gem/avis-fvs.o -o fire-gem/avis-fvs
 
 nasm -f elf64 fire-gem/fire-macro.asm -o fire-gem/fire-macro.o
 ld fire-gem/fire-macro.o -o fire-gem/fire-macro
 
-# The Engine (fire-gem.exe) will now find these in the path
+# Now run the Master Engine
 ./fire-gem/fire-gem.exe
