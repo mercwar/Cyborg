bf83a740645f6345e8d0c15f0b75ef59d759e6b0
CVBGOD <mercwar01@gmail.com>
2026-02-13 17:12:14 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -7,7 +7,7 @@ LOG="fire-gem/fire-gem.log"
 echo "[AVIS-SH] Identity: fire-gem-sh" >> "$LOG"
 echo "[AVIS-SH] Status: Shell Loader Active" >> "$LOG"
 
-# ASM HANDOFF - Kept commented out for this step
+# ASM HANDOFF - Commented out for verification
 # nasm -f elf64 fire-gem/fire-gem.asm -o fire-gem/fire-gem.o
 # ld fire-gem/fire-gem.o -o fire-gem/fire-gem.exe
 # ./fire-gem/fire-gem.exe
