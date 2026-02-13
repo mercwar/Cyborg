616e6cacd976c852f05a099b9b7292a7b803188c
CVBGOD <mercwar01@gmail.com>
2026-02-13 17:22:45 -0500

    Update fire-gem.asm

--- fire-gem/fire-gem.asm
+++ fire-gem/fire-gem.asm
@@ -1,29 +1,28 @@
 section .data
-    dir1 db "fire-gem/deployed", 0
-    file1 db "fire-gem/deployed/hello.txt", 0
-    msg db "hello from fire-gem", 0xa
-    len equ $ - msg
+    log_path db "fire-gem/fire-gem.log", 0
+    msg      db "[AVIS-ASM] Identity: fire-gem-asm -> hello", 0xa
+    len      equ $ - msg
 
 section .text
     global _start
-_start:
-    mov rax, 83         ; sys_mkdir
-    mov rdi, dir1
-    mov rsi, 0755o
-    syscall
 
+_start:
+    ; Open log: O_CREAT (64) | O_WRONLY (1) | O_APPEND (1024) = 1089
     mov rax, 2          ; sys_open
-    mov rdi, file1
-    mov rsi, 65         ; O_CREAT | O_WRONLY
+    mov rdi, log_path
+    mov rsi, 1089       ; Append mode
     mov rdx, 0644o
     syscall
-
-    mov rdi, rax
+    
+    ; Save File Descriptor and Write
+    mov rdi, rax        
     mov rax, 1          ; sys_write
     mov rsi, msg
     mov rdx, len
     syscall
 
-    mov rax, 60         ; Exit builder
+    ; Exit
+    mov rax, 60         ; sys_exit
     xor rdi, rdi
     syscall
+
